d13e5438b15870ecd03458e503466fb08a57aaea
lijiahao@youlai.cn <lijiahao@youlai.cn>
2022-08-16 23:07:21 +0800

    上线

---
+++ push.sh
@@ -0,0 +1,12 @@
+###
+ # @Author: lijiahao@youlai.cn lijiahao@youlai.cn
+ # @Date: 2022-08-16 22:34:07
+ # @LastEditors: lijiahao@youlai.cn lijiahao@youlai.cn
+ # @LastEditTime: 2022-08-16 22:34:44
+ # @FilePath: /fronted_end/push.sh
+ # @Description: 
+### 
+# !/usr/bin/env sh
+git add . 
+git commit -m '上线'
+git push -f https://github.com/qiyulaoshi/fronted_end.git master
